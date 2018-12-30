d8276d704af0e6dc9b50eeeec1d154eab55ce784
Oleh <oleg-mbo@ukr.net>
2018-12-31 00:01:39 +0200

    add types

---
+++ types.go
@@ -0,0 +1,21 @@
+package radiobot
+
+import (
+	"github.com/google/uuid"
+	telebot "gopkg.in/tucnak/telebot.v2"
+)
+
+// Channel represent radio channel
+type Channel struct {
+	ID      uuid.UUID `json:"id" bson:"_id"`
+	Name    string
+	Comment string
+}
+
+// Chat telegram info
+type Chat struct {
+	telebot.Chat
+	ID int64 `json:"id" bson:"_id"`
+
+	SubscribedChannels []Channel
+}
