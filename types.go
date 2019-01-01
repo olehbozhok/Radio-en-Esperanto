c3a98dc6b3e3dc20fd3d70ec9ea68a367155cf69
Oleh <oleg-mbo@ukr.net>
2019-01-01 21:38:26 +0200

    copy and paste telebot.Chat to Chat type

--- types.go
+++ types.go
@@ -1,6 +1,8 @@
 package radiobot
 
 import (
+	"strconv"
+
 	"github.com/google/uuid"
 	telebot "gopkg.in/tucnak/telebot.v2"
 )
@@ -14,8 +16,25 @@ type Channel struct {
 
 // Chat telegram info
 type Chat struct {
-	telebot.Chat
 	ID int64 `json:"id" bson:"_id"`
 
+	// See telebot.ChatType and consts.
+	Type telebot.ChatType `json:"type"`
+
+	// Won't be there for ChatPrivate.
+	Title string `json:"title"`
+
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Username  string `json:"username"`
+
 	SubscribedChannels []*Channel
+}
+
+// Recipient returns chat ID (see Recipient interface).
+func (c *Chat) Recipient() string {
+	if c.Type == telebot.ChatChannel {
+		return "@" + c.Username
+	}
+	return strconv.FormatInt(c.ID, 10)
 }
