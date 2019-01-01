892a9273aaa35f571b277c247825446916a4ce49
Oleh <oleg-mbo@ukr.net>
2019-01-01 22:58:56 +0200

    implement mongoChatRepository RegisterChat and some refactor

--- repository/mongoChat.go
+++ repository/mongoChat.go
@@ -15,36 +15,36 @@ func NewMongoChatRepository(collection *mgo.Collection) radiobot.ChatRepository 
 }
 
 // RegisterChat is used for register chats
-func (mchat *mongoChatRepository) RegisterChat(*radiobot.Chat) error {
-	panic("implement me")
+func (rchat *mongoChatRepository) RegisterChat(chat *radiobot.Chat) error {
+	return rchat.Collection.Insert(chat)
 }
 
 // FindChat is used to find chat by id
-func (mchat *mongoChatRepository) FindChat(id int64) (*radiobot.Chat, error) {
+func (rchat *mongoChatRepository) FindChat(id int64) (*radiobot.Chat, error) {
 	panic("implement me")
 }
 
 // SubscribeChat is used to subscribe chat on channel
-func (mchat *mongoChatRepository) SubscribeChat(chat *radiobot.Chat, channel *radiobot.Channel) error {
+func (rchat *mongoChatRepository) SubscribeChat(chat *radiobot.Chat, channel *radiobot.Channel) error {
 	panic("implement me")
 }
 
 // UnsubscribeChat is used to unsubscribe chat on channel
-func (mchat *mongoChatRepository) UnsubscribeChat(*radiobot.Chat, *radiobot.Channel) error {
+func (rchat *mongoChatRepository) UnsubscribeChat(*radiobot.Chat, *radiobot.Channel) error {
 	panic("implement me")
 }
 
 // GetAllChats is used to get all chats with count and offset
-func (mchat *mongoChatRepository) GetAllChats(count, offset int) ([]*radiobot.Chat, error) {
+func (rchat *mongoChatRepository) GetAllChats(count, offset int) ([]*radiobot.Chat, error) {
 	panic("implement me")
 }
 
 // GetAllChatsSubscribedOn is used to fetch all chats which subscribed on channel
-func (mchat *mongoChatRepository) GetAllChatsSubscribedOn(ch *radiobot.Channel, count, offset int) ([]*radiobot.Chat, error) {
+func (rchat *mongoChatRepository) GetAllChatsSubscribedOn(ch *radiobot.Channel, count, offset int) ([]*radiobot.Chat, error) {
 	panic("implement me")
 }
 
 // GetAllChatsIDSubscribedOn is used to fetch all chats ID which subscribed on channel
-func (mchat *mongoChatRepository) GetAllChatsIDSubscribedOn(ch *radiobot.Channel, count, offset int) ([]int64, error) {
+func (rchat *mongoChatRepository) GetAllChatsIDSubscribedOn(ch *radiobot.Channel, count, offset int) ([]int64, error) {
 	panic("implement me")
 }
